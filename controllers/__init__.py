cc272f18c026317de30af31a87fd24a2aa8c127e
folaringbolahan <folaringbolahan@gmail.com>
2025-11-26 12:23:15 +0100

    update laterna api - add coupon

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -7,6 +7,7 @@ from . import products_api
 from . import cart_api
 from . import best_selling_products_api
 from . import portal_auth_signup
+from . import coupon_api
 from . import api_controller
 from . import get_profile
 from . import product_category_api
